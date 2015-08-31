d581a65d7b2e5c16702beecacc5b93c2cb4d8ce7
Richard Mageau <whacksplat@gmail.com>
2015-08-31 10:07:59 -0400

    Added ConnectionScene, DataSourceScene, EndScene, VariableScene. remove Name field from AlwaysScene.

--- DirectorAPI/Scenes/ConnectionScene.cs
+++ DirectorAPI/Scenes/ConnectionScene.cs
@@ -1,63 +1,41 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
-using DirectorAPI.Actions.Notifications;
-using DirectorAPI.Conditions;
 using DirectorAPI.Interfaces;
 
 namespace DirectorAPI.Scenes
 {
-    public class AlwaysScene : IScene
+    public class ConnectionScene : IScene
     {
         private List<ICondition> conditions = new List<ICondition>();
 
-        public string Name{ get; set; }
-        
-        [ReadOnly(true)]
+        public string Name { get; set; }
         public Guid AutomationId { get; set; }
-        
-        [ReadOnly(true)]
         public Guid SceneId { get; set; }
-        [ReadOnly(true)]
         public int SortId { get; set; }
         public string CheckPoint { get; set; }
         public string CheckPointFailureStep { get; set; }
         public int Timeout { get; set; }
         public string TimeoutScene { get; set; }
-        
-        [ReadOnly(true)]
         public SceneEnums.SceneType Type { get; set; }
         public ICondition AddCondition(ICondition condition)
         {
-            //Always scene will only have singular Always condition.
-            if (conditions.Count > 0)
-            {
-                throw new Exception("Cannot have more than one condition in an Always scene.");
-            }
-            if (!(condition is AlwaysCondition))
-            {
-                throw new Exception("Cannot add anything but an AlwaysCondtion to an Always scene.");
-            }
-            
+            throw new NotImplementedException();
+
+            //only use Connection based conditions!
             condition.ConditionId = Guid.NewGuid();
             condition.SceneId = SceneId;
             condition.Type = condition.Type;
-            DBHelper.SaveCondition(this,condition);
+            DBHelper.SaveCondition(this, condition);
             conditions.Add(condition);
             return condition;
         }
 
         public List<ICondition> GetConditions()
         {
-            //return DBHelper.GetConditions(this.SceneId);
-            return conditions;
+            throw new NotImplementedException();
         }
     }
 }
---
+++ DirectorAPI/Scenes/VariableScene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectorAPI.Interfaces;
+
+namespace DirectorAPI.Scenes
+{
+    public class VariableScene : IScene
+    {
+        public string Name { get; set; }
+        public Guid AutomationId { get; set; }
+        public Guid SceneId { get; set; }
+        public int SortId { get; set; }
+        public string CheckPoint { get; set; }
+        public string CheckPointFailureStep { get; set; }
+        public int Timeout { get; set; }
+        public string TimeoutScene { get; set; }
+        public SceneEnums.SceneType Type { get; set; }
+        public ICondition AddCondition(ICondition condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ICondition> GetConditions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
