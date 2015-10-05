ff96d3087e153f2669d16d6a27efbf7592489694
Richard Mageau <whacksplat@gmail.com>
2015-10-05 14:19:46 -0400

    making sure this is added to svn

---
+++ DirectorAPI/Interfaces/ICharacterConnection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DirectorAPI.Interfaces
+{
+    public interface ICharacterConnection
+    {
+        //properties
+        string BufferTerminationCharacters { get; set; }
+        int Rows { get; set; }
+        int Columns { get; set; }
+        List<string> ScreenData { get; }
+
+        //methods
+        void Connect();
+        void Send(string input);
+        string Scrape(int Row, int Column, int Length); 
+    }
+}
